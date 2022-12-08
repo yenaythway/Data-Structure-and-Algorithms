18e9c52b42766863b81165411e1f8ee00d0f859a
Ye Nay Thway <102862836+yenaythway@users.noreply.github.com>
2022-12-08 08:43:46 +0630

    Update sorting_linked_list_node.c

--- sorting_linked_list_node.c
+++ sorting_linked_list_node.c
@@ -21,16 +21,15 @@ void printdata(struct node *myNode){
 void sort(struct node *myNode){
     int temp=0;
     struct node *first=myNode;
-    struct node *second=myNode->next;
     while (first!=NULL){
-        struct node* sec=myNode->next;
-        while (sec!=NULL){
-            if((first->data)>(sec->data)){
+        struct node* second=myNode->next;
+        while (second!=NULL){
+            if((first->data)>(second->data)){
                 temp=first->data;
-                first->data=sec->data;
-                sec->data=temp;
+                first->data=second->data;
+                second->data=temp;
             }
-            sec = sec->next;
+            second = second->next;
         }
         first=first->next;
         myNode=myNode->next;
@@ -42,18 +41,33 @@ int main(){
     struct node *three=NULL;
     struct node *four=NULL;
     struct node *five=NULL;
+    struct node *six=NULL;
+    struct node *seven=NULL;
+    struct node *eight=NULL;
+    struct node *nine=NULL;
+    struct node *ten=NULL;
 
     one = (malloc(sizeof(struct node)));
     two = (malloc(sizeof(struct node)));
     three = (malloc(sizeof(struct node)));
     four = (malloc(sizeof(struct node)));
     five = (malloc(sizeof(struct node)));
+    six = (malloc(sizeof(struct node)));
+    seven = (malloc(sizeof(struct node)));
+    eight = (malloc(sizeof(struct node)));
+    nine = (malloc(sizeof(struct node)));
+    ten = (malloc(sizeof(struct node)));
 
     one->next=two;
     two->next=three;
     three->next=four;
     four->next=five;
-    five->next=NULL;
+    five->next=six;
+    six->next=seven;
+    seven->next=eight;
+    eight->next=nine;
+    nine->next=ten;
+    ten->next=NULL;
 
     srand(time(NULL));
     addrandnumtonode(one);
